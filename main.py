b7093f95d79fd93a665c0f57d2670a431724c8be
aiexz <42418433+aiexz@users.noreply.github.com>
2020-04-22 00:03:17 +0300

    Решение таблиц

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import json
 JSON = input("Введите значение json: ")
 raw_json = json.loads(JSON.replace("\\", '\\\\').replace('\n', '\\n').replace('\t', '\\t').replace("\\\"","»"))
 for i in raw_json:
-	#print(i)
 	answer_type = i["answer"]["type"]
 	if answer_type == "answer/single":
 		right_answer = i['answer']['right_answer']['id']
@@ -19,11 +18,26 @@ for i in raw_json:
 				print("Задание " + str(i['taskNum']) + ") " + str(f['text']))
 	elif answer_type == "answer/match":
 		right_answer = i['answer']['right_answer']['match']
-		print(right_answer)
+		for id in right_answer:
+			task_content = ""
+			task_correct_answer = ""
+			for answer_variant in i['answer']['options']:
+				if answer_variant['id'] == id:
+					if answer_variant['text'] != "":
+						task_content = answer_variant['text']
+					else:
+						for array in answer_variant['content']:
+							task_content = array['content']
+				if answer_variant['id'] == right_answer[id][0]:
+					if answer_variant['text'] != "":
+						task_correct_answer = answer_variant['text']
+					else:
+						for array in answer_variant['content']:
+							task_correct_answer = array['content']
+			print("Задание " + str(i['taskNum']) + ") " + str(task_content) + " - " + str(task_correct_answer))
 	elif answer_type == "answer/number":
 		right_answer = i['answer']['right_answer']['number']
 		print("Задание " + str(i['taskNum']) + ") " + str(right_answer))
 	elif answer_type == "answer/string":
 		right_answer = i['answer']['right_answer']['string']
 		print("Задание " + str(i['taskNum']) + ") " + str(right_answer))
-		
